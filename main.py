be437c817c7b8a56c6bb48211a061b7fefece2e7
Carlos Joaquim <cjmota.cjmota@gmail.com>
2026-03-03 23:02:18 -0300

    fix railway python path

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from src.dashboard import app
 import os
+import sys
+
+# garante que o Python enxergue a pasta src
+sys.path.append(os.path.dirname(__file__))
+
+from src.dashboard import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
