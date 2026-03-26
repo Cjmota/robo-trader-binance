8bcfb18201484d044055da4007b8559a8af62614
Carlos Joaquim <cjmota.cjmota@gmail.com>
2026-03-26 09:48:17 -0300

    sua alteração

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,20 @@
+import json
 import os
 
+# 📁 pega o caminho base do projeto
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 📁 caminho do config
 CONFIG_FILE = os.path.join(BASE_DIR, "app", "config.json")
+
+
+def load_config():
+    try:
+        print(f"📁 Carregando config de: {CONFIG_FILE}")
+
+        with open(CONFIG_FILE, "r") as f:
+            return json.load(f)
+
+    except Exception as e:
+        print(f"❌ Erro ao carregar config: {e}")
+        return {}
